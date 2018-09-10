36a4a8a0d5eb1254b145f31c279fed358ed34365
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2018-09-10 19:43:13 +0100

    updated neural network script

--- neural_network.py
+++ neural_network.py
@@ -3,6 +3,9 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
+import time
 
 class NeuralNet(object):
     """This class provides method to perform classification of data using
@@ -12,7 +15,7 @@ n_iter       : number of iteration of forward/backward propagation
 lamb         : lambda parameter for regularization
 hidden_size  : hidden layer size
 n_labels     : number of labels, i.e. number of output classes"""
-    def __init__(self, alpha=0.001, n_iter=400, lamb=1, hidden_size, n_labels):
+    def __init__(self, alpha=1, n_iter=400, lamb=1, hidden_size=None, n_labels=None):
         self.alpha = alpha
         self.n_iter = n_iter
         self.lamb = lamb
@@ -21,7 +24,7 @@ n_labels     : number of labels, i.e. number of output classes"""
 
     def rand_weights(self, c_in, c_out, epsilon=0.12):
         """Return randomly initialized weights of a layer with c_in incoming connections and c_out and c_out outgoing connection."""
-        return np.random.random((c_out, c_in)) * 2 * epsilon - epsilon
+        return np.random.random((c_out, c_in + 1)) * 2 * epsilon - epsilon
 
     def add_intercept(self, X):
         """Return matrix with an added column of ones."""
@@ -34,17 +37,18 @@ n_labels     : number of labels, i.e. number of output classes"""
 
     def _sgrad(self, z):
         """Return the gradient of sigmoid function with z as input."""
-        return self_s(z) * (1 - self._s(z))
+        return self._s(z) * (1 - self._s(z))
 
     def forward_prop(self, X, Y, Theta1, Theta2):
         """Return values calculated during forward propagation and cost."""
-        X_1 = self.add_intercept(X)
-        z_2 = np.dot(X_1, Theta1)
+        z_2 = np.dot(X, Theta1.T)
         a_2 = self._s(z_2)
-        z_3 = np.dot(a_2, Theta2)
+        a_2 = self.add_intercept(a_2)
+        z_3 = np.dot(a_2, Theta2.T)
         h = self._s(z_3)
 
         #calculate unregularized cost
+        m = X.shape[0]
         J = np.sum(np.sum(Y * np.log(h) + (1 - Y) * np.log(1 - h))) / -m
         #calculate regularization parameters
         T1 = Theta1[:, 1:]
@@ -55,21 +59,22 @@ n_labels     : number of labels, i.e. number of output classes"""
         #calculate regularized cost
         J_reg = J + A * (B + C)
 
-        return z_2, a_2, z_3, h, J
+        return z_2, a_2, z_3, h, J_reg
 
-    def backward_prop(self, z_2, a_2, z_3, h, Y, Theta1, Theta2, lamb, alpha):
+    def backward_prop(self, z_2, a_2, z_3, h, X, Y, Theta1, Theta2, lamb, alpha):
         """Return updated Theta1 and Theta2."""
         #perform backward propagation per se
         m = Y.shape[0]
+        T2 = Theta2[:, 1:]
         d_3 = h - Y
         d_2 = np.dot(d_3, T2) * self._sgrad(z_2)
         Delta_1 = np.dot(d_2.T, X)
-        Delta_2 = np.dot(d_3, a_2)
+        Delta_2 = np.dot(d_3.T, a_2)
 
         #calculate unregularized gradient
         Theta1_grad = Delta_1 / m
         Theta2_grad = Delta_2 / m
- 
+
         #regularization of gradient
         #replace first column with 0 to avoid regularization of bias
         Theta1_copy = np.copy(Theta1)
@@ -85,3 +90,88 @@ n_labels     : number of labels, i.e. number of output classes"""
         Theta2_updated = Theta2 - alpha * Theta2_grad_reg
 
         return Theta1_updated, Theta2_updated
+
+    def predict(self, Theta1, Theta2, X, Y):
+        """Return predicted output and accuracy of model."""
+        m = X.shape[0]
+
+        #add intercept to X
+        X_1 = self.add_intercept(X)
+
+        #calculate outputs of neural network layers
+        h1 = self._s(np.dot(X_1, Theta1.T))
+        h1_1 = self.add_intercept(h1)
+        h2 = self._s(np.dot(h1_1, Theta2.T))
+
+        #predicted output (remember indexing starts at zero)
+        #prediction must have the same shape as Y
+        prediction = (np.argmax(h2, axis=1) + 1)[:, np.newaxis]
+
+        #accuracy
+        accuracy = np.mean(prediction == Y)
+
+        return prediction, accuracy
+
+    def fit(self, X, Y):
+        """Train the neural network."""
+        #add intercept to X
+        X_1 = self.add_intercept(X)
+
+        #get network architecture parameters
+        in_size = X.shape[1] #input layer size
+        hid_size = self.hidden_size #hidden layer size
+        n_lab = self.n_labels #output layer size
+
+        #recode Y into one-hot labels
+        Y_1 = np.zeros((Y.shape[0], n_lab))
+        for i in range(Y.shape[0]):
+            Y_1[i, Y.item(i) - 1] = 1
+        #could import OneHotEncoder from sklearn.preprocessing
+        #then Y_1 = OneHotEncoder(sparse=False).fit_transform(Y)
+        
+        #initialize Theta matrices randomly
+        Theta1 = self.rand_weights(in_size, hid_size, 0.12)
+        Theta2 = self.rand_weights(hid_size, n_lab, 0.12)
+
+        #define array to store cost history
+        cost_history = np.zeros(self.n_iter)
+
+        #train the neural network by repetition of for and back prop
+        for i in range(self.n_iter):
+            #for prop
+            z_2, a_2, z_3, h, J_reg = self.forward_prop(X_1, Y_1, Theta1, Theta2)
+            cost_history[i] = J_reg
+
+            #back prop
+            Theta1, Theta2 = self.backward_prop(z_2, a_2, z_3, h, X_1, Y_1, Theta1, Theta2, self.lamb, self.alpha)
+
+        return Theta1, Theta2, cost_history
+
+    def plot_history(self, history):
+        "Plot cost history to check error of parameters is minimized."""
+        fig, ax = plt.subplots()
+        ax.plot([i for i in range(self.n_iter)], history)
+        ax.set_xlabel("Number of iteration")
+        ax.set_ylabel("Cost")
+        ax.set_title("Cost history of neural network parameters")
+        plt.show()
+
+if __name__ == "__main__":
+    #import data and build input and build input and output matrices
+    data_in = pd.read_csv("digits.csv", header=None)
+    data_out = pd.read_csv("digits_output.csv", header=None)
+    X = np.array(data_in)
+    Y = np.array(data_out)
+
+    #define model, n_iter and alpha determined by trial and error
+    model = NeuralNet(hidden_size=25, n_labels=10, n_iter=400)
+
+    #train the model
+    #measure execution time
+    start = time.time()
+    Theta1, Theta2, cost_history = model.fit(X, Y)
+    stop = time.time()
+    print("Time to train the neural network :", str(stop - start))
+
+    #determine accurary of the model
+    pred, accu = model.predict(Theta1, Theta2, X, Y)
